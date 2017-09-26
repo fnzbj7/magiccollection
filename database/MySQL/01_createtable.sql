b91893942fe35cb430028bebc5f9153c863a22da
Arpas Milan <jukijun91@gmail.com>
2017-09-26 09:18:39 +0200

    Start commit

---
+++ database/MySQL/01_createtable.sql
@@ -0,0 +1,37 @@
+CREATE TABLE CardExpansion (
+CardExpansionID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+CardExpansionName VARCHAR(30) NOT NULL,
+CardExpansionShortName VARCHAR(4) NOT NULL
+);
+
+CREATE TABLE Card (
+CardID INT(3) UNSIGNED NOT NULL,
+CardExpansion_1 INT(6) UNSIGNED NOT NULL,
+CardName VARCHAR(40) NOT NULL,
+Rarity VARCHAR(3),
+PRIMARY KEY(CardID,CardExpansion_1),
+FOREIGN KEY (CardExpansion_1) REFERENCES CardExpansion(CardExpansionID)
+);
+
+CREATE TABLE Player (
+PlayerID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+PlayerName VARCHAR(30) NOT NULL
+); 
+
+CREATE TABLE CardAmount (
+Card_1 INT(6) UNSIGNED NOT NULL,
+CardExpansion_1 INT(6) UNSIGNED NOT NULL,
+Player_1 INT(6) UNSIGNED NOT NULL,
+Amount INT(2),
+PRIMARY KEY(Card_1,CardExpansion_1,Player_1),
+FOREIGN KEY (Card_1) REFERENCES Card(CardID),
+FOREIGN KEY (Player_1) REFERENCES Player(PlayerID),
+FOREIGN KEY (CardExpansion_1) REFERENCES CardExpansion(CardExpansionID)
+);
+
+CREATE TABLE UploadFile (
+FileName VARCHAR(50),
+CardExpansion_1 INT(6) UNSIGNED NOT NULL,
+PRIMARY KEY(FileName,CardExpansion_1),
+FOREIGN KEY (CardExpansion_1) REFERENCES CardExpansion(CardExpansionID)
+); 
---
+++ database/MySQL/02_insertCardExpansions.sql
@@ -0,0 +1,9 @@
+INSERT INTO CardExpansion (CardExpansionName,CardExpansionShortName) values('Ixalan','XLN');
+INSERT INTO CardExpansion (CardExpansionName,CardExpansionShortName) values('Hour of Devastation','HOU');
+INSERT INTO CardExpansion (CardExpansionName,CardExpansionShortName) values('Amonkhet','AKH');
+INSERT INTO CardExpansion (CardExpansionName,CardExpansionShortName) values('Aether Revolt','AER');
+INSERT INTO CardExpansion (CardExpansionName,CardExpansionShortName) values('Kaladesh','KLD');
+INSERT INTO CardExpansion (CardExpansionName,CardExpansionShortName) values('Eldritch Moon','EMN');
+INSERT INTO CardExpansion (CardExpansionName,CardExpansionShortName) values('Shadows over Innistrad','SOI');
+INSERT INTO CardExpansion (CardExpansionName,CardExpansionShortName) values('Oath of the Gatewatch','OGW');
+INSERT INTO CardExpansion (CardExpansionName,CardExpansionShortName) values('Battle For Zendikar','BFZ');
