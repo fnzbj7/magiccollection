136824c1b05bd2fcc4757f924803f28f96b6ca1f
fnzbj7 <jukijun91@gmail.com>
2017-10-15 13:30:21 +0200

    Database connection file structure changes

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-var mysql = require('mysql')
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'admin',
-  database : 'magic'
-});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	var connection = require('./db');
 	var row;
 	var sqlQuery
 	var isCUC=true;
@@ -20,7 +14,7 @@ router.get('/', function(req, res, next) {
 			'join cardexpansion ce on ca.CardExpansion_1 = ce.CardExpansionID '+
 			'join card c on c.CardID = ca.Card_1 and c.CardExpansion_1 = ca.CardExpansion_1 '+
 			'where ca.CardExpansion_1 = 9 and player_1 = 1 and player_1 = 1 '+
-			"and (c.Rarity != 'R' and c.Rarity != 'M')"+
+			"and (c.Rarity != 'R' and c.Rarity != 'M') "+
 			'order by ca.card_1, ce.CardExpansionShortName LIMIT 35 OFFSET 0;';
 	}else{
 		sqlQuery = 'select ca.card_1, ce.CardExpansionShortName, ca.Amount, c.Rarity, c.Doubleside '+
@@ -32,13 +26,10 @@ router.get('/', function(req, res, next) {
 	}
 	connection.query( sqlQuery , function (err, rows, fields) {
 		
-		if (err) throw err
+		if (err) throw err;
 		row = rows
-		//console.log('Almafa:', row);
-		//console.log('The solution is: ', rows);
-		
 		res.locals.Test = "Teszt";
-	  res.render('index', { title: 'Nice one', workflowData: JSON.stringify(row) });
+		res.render('index', { title: 'Nice one', workflowData: JSON.stringify(row) });
 	});
 
 });
