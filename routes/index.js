06a54001512e3f08e59976884f21ab163a4fa90e
Milán Árpás <jukijun91@gmail.com>
2018-01-16 23:41:52 +0100

    Add Rivals of Ixalan

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ router.get('/', function(req, res, next) {
 			'from cardamount ca '+
 			'join cardexpansion ce on ca.CardExpansion_1 = ce.CardExpansionID '+
 			'join card c on c.CardID = ca.Card_1 and c.CardExpansion_1 = ca.CardExpansion_1 '+
-			'where ca.CardExpansion_1 = 9 and player_1 = 1 and player_1 = 1 '+
+			"where ca.CardExpansion_1 = (select CardExpansionID from cardexpansion where CardExpansionShortName = 'AER') and player_1 = 1 and player_1 = 1 "+
 			"and (c.Rarity != 'R' and c.Rarity != 'M') "+
 			'order by ca.card_1, ce.CardExpansionShortName LIMIT ' + cardLimit +' OFFSET 0;';
 	}else{
@@ -22,11 +22,11 @@ router.get('/', function(req, res, next) {
 			'from cardamount ca '+
 			'join cardexpansion ce on ca.CardExpansion_1 = ce.CardExpansionID '+
 			'join card c on c.CardID = ca.Card_1 and c.CardExpansion_1 = ca.CardExpansion_1 '+
-			'where ca.CardExpansion_1 = 9 and player_1 = 1 and player_1 = 1 '+
+			"where ca.CardExpansion_1 = (select CardExpansionID from cardexpansion where CardExpansionShortName = 'AER') and player_1 = 1 and player_1 = 1 "+
 			'order by ca.card_1, ce.CardExpansionShortName LIMIT ' + cardLimit +' OFFSET 0;';
 	}
 	
-	sqlQuery += 'Select count(*) as lim from card ca where CardExpansion_1 = 9;';
+	sqlQuery += "Select count(*) as lim from card ca where CardExpansion_1 = (select CardExpansionID from cardexpansion where CardExpansionShortName = 'AER')";
 	connection.query( sqlQuery , function (err, rows, fields) {
 		
 		if (err) throw err;
