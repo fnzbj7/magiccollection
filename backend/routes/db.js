cf6087693c20e068ec9fbb50955c96116bed94f5
Árpás Milán <jukijun91@gmail.com>
2019-01-24 00:02:00 +0100

    Database changes

--- backend/routes/db.js
+++ backend/routes/db.js
@@ -13,33 +13,32 @@ var dbconfig = require("./dbconfig");
 }
 
 */
+var pool = mysql.createPool(dbconfig.db);
 
-var connection;
-
-function handleDisconnect() {
-
-connection = mysql.createConnection(dbconfig.db);
-
-  connection.connect(function(err) {              
-    if(err) {
-      console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 5000);
+module.exports = {
+    query: function(){
+        var sql_args = [];
+        var args = [];
+        for(var i=0; i<arguments.length; i++){
+            args.push(arguments[i]);
+        }
+        var callback = args[args.length-1]; //last arg is callback
+        pool.getConnection(function(err, connection) {
+        if(err) {
+                console.log(err);
+                return callback(err);
+            }
+            if(args.length > 2){
+                sql_args = args[1];
+            }
+        connection.query(args[0], sql_args, function(err, results) {
+          connection.release(); // always put connection back in pool after last query
+          if(err){
+                    console.log(err);
+                    return callback(err);
+                }
+          callback(null, results);
+        });
+      });
     }
-  });
-
-  connection.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST' || err.code === 'PROTOCOL_CONNECTION_LOST') { 
-      setTimeout(handleDisconnect, 10000);
-    } else {
-      throw err;
-    }
-  });
-  
-  module.exports = connection;
-}
-
-handleDisconnect();
-
-
-
+};
