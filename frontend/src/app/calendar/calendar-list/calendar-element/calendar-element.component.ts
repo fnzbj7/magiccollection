9b864856f6931dc7c2684581b64dbd97157ea7be
Árpás Milán <jukijun91@gmail.com>
2019-03-01 15:33:47 +0100

    Calendar development, red click event

--- frontend/src/app/calendar/calendar-list/calendar-element/calendar-element.component.ts
+++ frontend/src/app/calendar/calendar-list/calendar-element/calendar-element.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { CalendarEvent } from '../model/calendar-event.model';
+import { CalendarService } from '../../calendar.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-calendar-element',
   templateUrl: './calendar-element.component.html',
   styleUrls: ['./calendar-element.component.css']
 })
-export class CalendarElementComponent implements OnInit {
+export class CalendarElementComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  @Input() calendarEvent: CalendarEvent;
+  selectCalendarEventSub: Subscription;
+  isSelected = false;
+
+  constructor(private calendarService: CalendarService) { }
 
   ngOnInit() {
+    this.isSelected = this.calendarEvent.id === this.calendarService.getSelectedEventId();
+    this.selectCalendarEventSub = this.calendarService.getselectCalendarEventSub().subscribe( selectedId => {
+      this.isSelected = selectedId === this.calendarEvent.id;
+    });
+  }
+
+  onSelectCalendarEvent() {
+    this.calendarService.selectCalendarEvent( this.calendarEvent );
+  }
+
+  ngOnDestroy() {
+    if (this.selectCalendarEventSub) {
+      this.selectCalendarEventSub.unsubscribe();
+    }
   }
 
 }
