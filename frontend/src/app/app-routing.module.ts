a7454b504331857d0682a883a70dbbbcf5a1af35
Árpás Milán <jukijun91@gmail.com>
2019-03-02 10:35:49 +0100

    Fix backend error throw + Cardselect miss

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MagicCardListComponent } from './magic/magic-card-list/magic-card-list.component';
+import { MagicExpansionListComponent } from './magic/magic-expansion-list/magic-expansion-list.component';
 
 const appRoute: Routes = [
     { path: 'calendar', loadChildren: './calendar/calendar.module#CalendarModule'},
     { path: ':expansion', component: MagicCardListComponent},
+    //{ path: '', redirectTo: '/RNA', pathMatch: 'full'}
+    { path: '', component: MagicExpansionListComponent, pathMatch: 'full'}
 ];
 
 @NgModule({
