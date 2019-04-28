f48e01dc48f2e994c4cb0d4fa6a5f8d92ec26152
Árpás Milán <jukijun91@gmail.com>
2019-04-28 20:32:27 +0200

    Image refactor and WAR set added

--- frontend/src/app/magic/magic-expansion-list/magic-expansion-list.component.ts
+++ frontend/src/app/magic/magic-expansion-list/magic-expansion-list.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MagicExpansionListComponent implements OnInit {
 
-  cardSetsArray: string[] = ['RNA', 'GRN', 'M19', 'DOM', 'RIX', 'XLN', 'HOU', 'AKH', 'AER', 'KLD', 'EMN', 'SOI', 'OGW', 'BFZ'];
+  cardSetsArray: string[] = ['WAR', 'RNA', 'GRN', 'M19', 'DOM', 'RIX', 'XLN', 'HOU', 'AKH', 'AER', 'KLD', 'EMN', 'SOI', 'OGW', 'BFZ'];
   expansion: string;
   isOpenMenu = false;
 
