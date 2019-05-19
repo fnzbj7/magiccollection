b734e20922c80150a8554eb0ec347352203991e2
Árpás Milán <jukijun91@gmail.com>
2019-05-19 23:31:13 +0200

    Backend use promises

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,4 +1,4 @@
-var mysql = require('mysql');
+var mysql = require("mysql");
 var dbconfig = require("./dbconfig");
 /* Create a dbconfig.json file next to this file, and contain something like this:
 
@@ -16,29 +16,32 @@ var dbconfig = require("./dbconfig");
 var pool = mysql.createPool(dbconfig.db);
 
 module.exports = {
-    query: function(){
-        var sql_args = [];
-        var args = [];
-        for(var i=0; i<arguments.length; i++){
-            args.push(arguments[i]);
+  query: function() {
+    var sql_args = [];
+    var args = [];
+    for (var i = 0; i < arguments.length; i++) {
+      args.push(arguments[i]);
+    }
+    return new Promise((resolve, reject) => {
+      pool.getConnection((err, connection) => {
+        if (err) {
+          console.log(err);
+          return reject('DbError');
         }
-        var callback = args[args.length-1]; //last arg is callback
-        pool.getConnection(function(err, connection) {
-        if(err) {
-                console.log(err);
-                return callback(err);
-            }
-            if(args.length > 2){
-                sql_args = args[1];
-            }
-        connection.query(args[0], sql_args, function(err, results) {
+        if (args.length > 1) {
+          sql_args = args[1];
+        }
+
+        connection.query(args[0], sql_args, (err, results) => {
           connection.release(); // always put connection back in pool after last query
-          if(err){
-                    console.log(err);
-                    return callback(err);
-                }
-          callback(null, results);
+          if (err) {
+            console.log(err);
+            reject('DbError');
+          } else {
+            resolve(results);
+          }
         });
       });
-    }
+    });
+  }
 };
