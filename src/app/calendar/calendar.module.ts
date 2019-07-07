3d0aa4a3fba44192be5da452a82e42fedb8cb1d6
Árpás Milán <jukijun91@gmail.com>
2019-07-07 07:45:10 +0200

    Mobile screen category border bug fix

--- src/app/calendar/calendar.module.ts
+++ src/app/calendar/calendar.module.ts
@@ -5,32 +5,16 @@ import { CalendarRoutingModule } from './calendar-routing.module';
 import { CalendarListComponent } from './calendar-list/calendar-list.component';
 import { CalendarElementComponent } from './calendar-list/calendar-element/calendar-element.component';
 import { CalendarService } from './calendar.service';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import {
-        faCoffee,
-        faAngleLeft,
-        faAngleRight,
-        faTimes,
-        faCalendarPlus
-      } from '@fortawesome/free-solid-svg-icons';
 import { EventDetailsComponent } from './calendar-list/event-details/event-details.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [CalendarListComponent, CalendarElementComponent, EventDetailsComponent],
   imports: [
     CommonModule,
     CalendarRoutingModule,
-    FontAwesomeModule
+    SharedModule
   ],
   providers: [CalendarService]
 })
-export class CalendarModule {
-  constructor() {
-    library.add(faCoffee);
-    library.add(faAngleLeft);
-    library.add(faAngleRight);
-    library.add(faTimes);
-    library.add(faCalendarPlus);
-  }
- }
+export class CalendarModule { }
