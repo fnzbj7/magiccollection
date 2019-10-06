7b98c9b98132933429077901465cae17491ca421
Árpás Milán <jukijun91@gmail.com>
2019-10-06 02:00:34 +0200

    Fix version service

--- frontend/src/environments/version.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-
-// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
-/* tslint:disable */
-@Injectable({providedIn: 'root'})
-export class VersionService {
-  public VERSION = "2019-10-03 19:54:22";
-}
-/* tslint:enable */
