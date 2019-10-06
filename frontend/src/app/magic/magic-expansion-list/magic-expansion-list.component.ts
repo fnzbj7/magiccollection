a5776c4e14e0defdc38fdc1f109a01048f3be1f5
Árpás Milán <jukijun91@gmail.com>
2019-10-06 03:02:03 +0200

    Squashed commit of the following:
    commit 95dd7528482ddec2fa64ba7ccd32d3e526c15a6d
    Author: Árpás Milán <jukijun91@gmail.com>
    Date:   Sun Oct 6 02:00:34 2019 +0200
    
        Fix version service
    
    commit ea793a1dc909bc172800970699c61e6a4c71ed5d
    Author: Árpás Milán <jukijun91@gmail.com>
    Date:   Sun Oct 6 01:58:20 2019 +0200
    
        Squashed commit of the following:
    
        commit cb8a894a8c65d5a85d85434eac19cbc5cc8548d0
        Author: Árpás Milán <jukijun91@gmail.com>
        Date:   Sun Oct 6 01:55:39 2019 +0200
    
            Add ELD cards
    
    commit 561dcbce0da76bf08dffcb61d2cdc79797bc16d2
    Author: Árpás Milán <jukijun91@gmail.com>
    Date:   Sat Oct 5 01:57:32 2019 +0200
    
        Squashed commit of the following:
    
        commit d70c3c7466acdaf1f42037bade3c13604551715e
        Author: Árpás Milán <jukijun91@gmail.com>
        Date:   Sat Oct 5 01:55:36 2019 +0200
    
            remove branch
    
    commit ab70fa5a3290364aa27a0d84f7f74c3492ddde5c
    Author: Árpás Milán <jukijun91@gmail.com>
    Date:   Sat Oct 5 01:32:03 2019 +0200
    
        Squashed commit of the following:
    
        commit 780e78e7e168cb8b725fafe6d169bb8435239f65
        Author: Árpás Milán <jukijun91@gmail.com>
        Date:   Fri Oct 4 00:37:48 2019 +0200
    
            Angular url Fix for production
    
        commit 5de3b765eb44d813a126f5c72f2da13926cee8c5
        Author: Árpás Milán <jukijun91@gmail.com>
        Date:   Thu Oct 3 23:50:06 2019 +0200
    
            Syncronous change to to in productioon
    
        commit 8526da88765fbf3ec413c8ddbbb71d624d036b9b
        Author: Árpás Milán <jukijun91@gmail.com>
        Date:   Thu Oct 3 22:43:25 2019 +0200
    
            Fix nestjs typeorm entities
    
        commit b7d9fb971c764ff0f99f2471db2acbdb83605175
        Author: Árpás Milán <jukijun91@gmail.com>
        Date:   Thu Oct 3 21:40:13 2019 +0200
    
            Adding config to nest.js
    
        commit 8f1014948cff180323e690c664f0a4082edd6ae9
        Author: Árpás Milán <jukijun91@gmail.com>
        Date:   Thu Oct 3 20:12:38 2019 +0200
    
            Package.json adding start script
    
        commit 13b93f1d89e3c6ede9b6c25e2830d9d8f0913c04
        Author: Árpás Milán <jukijun91@gmail.com>
        Date:   Thu Oct 3 20:07:16 2019 +0200
    
            Package.json add to install dev dependencies
    
        commit c2bc68b47160617f77337f5414ea6d69de92a962
        Author: Árpás Milán <jukijun91@gmail.com>
        Date:   Thu Oct 3 20:03:23 2019 +0200
    
            Some changes in package.json for Heroku deploy
    
        commit 717d32dfbd20fc4cd391d6626df764e5db0255da
        Author: Árpás Milán <jukijun91@gmail.com>
        Date:   Thu Oct 3 19:55:21 2019 +0200
    
            Preparing to deploy to production
    
        commit 5112844ff45627f41c563ff180ef9f117052d2ff
        Author: Árpás Milán <jukijun91@gmail.com>
        Date:   Sun Sep 29 19:50:45 2019 +0200
    
            Basic db connection establised
    
        commit f3b6cffd2db9c66852eb8b35ab30d0b16e91bce2
        Author: Árpás Milán <jukijun91@gmail.com>
        Date:   Mon Sep 16 00:26:22 2019 +0200
    
            Start commit for Nest.js

--- frontend/src/app/magic/magic-expansion-list/magic-expansion-list.component.ts
+++ frontend/src/app/magic/magic-expansion-list/magic-expansion-list.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MagicExpansionListComponent implements OnInit {
 
-  cardSetsArray: string[] = ['M20', 'WAR', 'RNA', 'GRN', 'M19', 'DOM', 'RIX', 'XLN', 'HOU', 'AKH', 'AER', 'KLD', 'EMN', 'SOI', 'OGW', 'BFZ'];
+  cardSetsArray: string[] = ['ELD','M20', 'WAR', 'RNA', 'GRN', 'M19', 'DOM', 'RIX', 'XLN', 'HOU', 'AKH', 'AER', 'KLD', 'EMN', 'SOI', 'OGW', 'BFZ'];
   expansion: string;
   isOpenMenu = false;
 
