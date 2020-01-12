74df731945afcb5669fb43afa8a5fa3308fa1d62
Árpás Milán <jukijun91@gmail.com>
2020-01-12 11:35:35 +0100

    Fixing getCard after logging out

--- frontend/src/app/auth/authentication.service.ts
+++ frontend/src/app/auth/authentication.service.ts
@@ -65,8 +65,8 @@ export class AuthenticationService {
     logout() {
         // remove user from local storage to log user out
         localStorage.removeItem('currentUser');
-        this.currentUserSubject.next(null);
         this.loggedIn = false;
+        this.currentUserSubject.next(null);
     }
 
     isLoggedIn() {
