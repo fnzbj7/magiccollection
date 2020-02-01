402299e447e68be194e0908753ca6c961f1717d0
Árpás Milán <jukijun91@gmail.com>
2020-02-01 21:38:32 +0100

    Adding sticky to landslide mobile view

--- frontend/src/app/magic/magic-expansion-list/magic-expansion-list.component.ts
+++ frontend/src/app/magic/magic-expansion-list/magic-expansion-list.component.ts
@@ -14,33 +14,12 @@ import { MagicCardsListService } from '../magic-cards-list.service';
     templateUrl: './magic-expansion-list.component.html',
     styleUrls: ['./magic-expansion-list.component.css'],
 })
-export class MagicExpansionListComponent implements OnInit, AfterViewInit {
+export class MagicExpansionListComponent implements OnInit {
     cardSetsArray: string[];
-    sticky = false;
-    menuPosition: number;
-
-    @ViewChild('stickyMenu', { static: true }) menuElement: ElementRef;
-    @HostBinding('class.card-set-mobil-landscape') mobilLandscape = false;
 
     constructor(private magicCardsListService: MagicCardsListService) {}
 
     ngOnInit() {
         this.cardSetsArray = this.magicCardsListService.cardSetsArray;
-    }
-
-    ngAfterViewInit() {
-        this.menuPosition = this.menuElement.nativeElement.offsetTop;
-    }
-
-    @HostListener('window:scroll', ['$event'])
-    handleScroll() {
-        const windowScroll = window.pageYOffset;
-        if (windowScroll >= this.menuPosition) {
-            this.sticky = true;
-            this.mobilLandscape = true;
-        } else {
-            this.sticky = false;
-            this.mobilLandscape = false;
-        }
     }
 }
