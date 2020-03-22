57e57141bd59ff2c5c726f0b3aaba025819d0d61
Árpás Milán <jukijun91@gmail.com>
2020-03-22 16:03:09 +0100

    Changeing the way nestjs decorators work

--- src/backend/auth/get-user.decorator.ts
+++ src/backend/auth/get-user.decorator.ts
@@ -1,8 +1,9 @@
-import { createParamDecorator } from '@nestjs/common';
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { User } from './entity/user.entity';
 
 export const GetUser = createParamDecorator(
-    (data, req): User => {
+    (data, ctx: ExecutionContext): User => {
+        const req = ctx.switchToHttp().getRequest();
         return req.user;
     },
 );
--- src/backend/auth/jwt.strategy.ts
+++ src/backend/auth/jwt.strategy.ts
@@ -20,7 +20,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     async validate(payload: JwtPayload) {
         const { email } = payload;
         const user = await this.userRepository.findOne({ email });
-
         if (!user) {
             throw new UnauthorizedException();
         }
