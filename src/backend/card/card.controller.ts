3b5796578482ec900d6b555f29589f0567d682db
Árpás Milán <jukijun91@gmail.com>
2020-03-22 11:58:53 +0100

    The big merge with backend and frontend. (There was some problem with Jest and Jasmin types with nest.js building, but i removed them)

---
+++ src/backend/card/card.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Param, UseGuards, Body } from '@nestjs/common';
+import { CardService } from './card.service';
+import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from '../auth/get-user.decorator';
+import { User } from '../auth/entity/user.entity';
+import { ModifyCardDto } from './dto/add-card.dto';
+
+@Controller('card')
+export class CardController {
+    constructor(private cardService: CardService) {}
+
+    @Get('/cardset/:set')
+    async getCardSet(@Param('set') cardSet: string) {
+        return await this.cardService.getCardSet(cardSet);
+    }
+
+    @Get('/cardsetuser/:set')
+    @UseGuards(AuthGuard())
+    async getCardSetUser(@Param('set') cardSet: string, @GetUser() user: User) {
+        return await this.cardService.getCardSetUser(cardSet, user);
+    }
+
+    @Post('/addcard')
+    @UseGuards(AuthGuard())
+    async addCard(@Body() addCard: ModifyCardDto, @GetUser() user: User): Promise<void> {
+        await this.cardService.addCard(addCard, user);
+    }
+
+    @Post('/removecard')
+    @UseGuards(AuthGuard())
+    async removeCard(@Body() removeCard: ModifyCardDto, @GetUser() user: User): Promise<void> {
+        await this.cardService.removeCard(removeCard, user);
+    }
+}
