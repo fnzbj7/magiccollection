7a77649d0841a7570a70c94587145352b389825d
Árpás Milán <jukijun91@gmail.com>
2020-04-11 22:53:43 +0200

    Adding Ikoria

--- src/frontend/app/magic/magic-card-list/magic-cards-list.service.ts
+++ src/frontend/app/magic/magic-card-list/magic-cards-list.service.ts
@@ -20,6 +20,7 @@ export class MagicCardsListService {
     quantityFilterSub = new BehaviorSubject<QuantityFilterEnum>(QuantityFilterEnum.ALL);
 
     cardSetsArray: string[] = [
+        'IKO',
         'THB',
         'ELD',
         'M20',
@@ -41,6 +42,7 @@ export class MagicCardsListService {
     ];
 
     maxCardNumber = {
+        IKO: 387,
         AER: 194,
         AKH: 287,
         BFZ: 274,
