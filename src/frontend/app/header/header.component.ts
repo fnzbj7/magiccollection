eacb177aeb42fc75f6346cc402be63758a9df15a
Árpás Milán <jukijun91@gmail.com>
2020-08-01 14:00:45 +0200

    Creating update icon

--- src/frontend/app/header/header.component.ts
+++ src/frontend/app/header/header.component.ts
@@ -1,35 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthComponent } from '../auth/auth.component';
 import { AuthenticationService } from '../auth/authentication.service';
 import { User } from '../model/user.model';
 import { VersionService } from '../../environments/version.service';
-import { faInfoCircle, faBars } from '@fortawesome/free-solid-svg-icons';
+import { faInfoCircle, faBars, faArrowCircleUp } from '@fortawesome/free-solid-svg-icons';
 import { MatDialog } from '@angular/material/dialog';
 import { SideMenuService } from '../shared/side-menu.service';
+import { UpdatePwaService } from '../auth/update-pwa.service';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
     loggedUser: User;
     version: string;
+    needUpdate = false;
 
     // Font-Aesome
     faInfoCircle = faInfoCircle;
+    faArrowCircleUp = faArrowCircleUp;
     faBars = faBars;
+
+    currentUserSub: Subscription;
+    needUpdateSub: Subscription;
 
     constructor(
         private dialog: MatDialog,
         private authenticationService: AuthenticationService,
         private versionService: VersionService,
         public sideMenuService: SideMenuService,
+        private updatePwaService: UpdatePwaService,
     ) {}
 
     ngOnInit() {
-        this.authenticationService.currentUserSubject.subscribe(user => {
+        this.currentUserSub = this.authenticationService.currentUserSubject.subscribe(user => {
             this.loggedUser = user;
+        });
+        this.needUpdateSub = this.updatePwaService.getNeedUpdateSubject().subscribe(needUpdate => {
+            this.needUpdate = needUpdate;
         });
         this.version = this.versionService.VERSION;
     }
@@ -39,7 +50,20 @@ export class HeaderComponent implements OnInit {
         // this.bsModalRef = this.modalService.show(AuthComponent);
     }
 
+    onUpdate() {
+        window.location.reload();
+    }
+
     onLogout() {
         this.authenticationService.logout();
+    }
+
+    ngOnDestroy() {
+        if (this.currentUserSub) {
+            this.currentUserSub.unsubscribe();
+        }
+        if (this.needUpdateSub) {
+            this.needUpdateSub.unsubscribe();
+        }
     }
 }
