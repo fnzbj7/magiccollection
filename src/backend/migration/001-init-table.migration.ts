2a8aa3f44fd4128c5d1810ce08bb6aa7a7bd7af4
Árpás Milán <jukijun91@gmail.com>
2020-09-05 13:59:51 +0200

    Adding first init script

---
+++ src/backend/migration/001-init-table.migration.ts
@@ -0,0 +1,45 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class InitTableMigration implements MigrationInterface {
+    name = 'initTable1599256555400';
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+            'CREATE TABLE `card_set` (`id` int NOT NULL AUTO_INCREMENT, `name` varchar(255) NOT NULL, `short_name` varchar(255) NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB',
+        );
+        await queryRunner.query(
+            'CREATE TABLE `card` (`id` int NOT NULL AUTO_INCREMENT, `card_number` int NOT NULL, `name` varchar(255) NOT NULL, `rarity` varchar(255) NOT NULL, `layout` varchar(255) NOT NULL, `card_set_1` int NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB',
+        );
+        await queryRunner.query(
+            'CREATE TABLE `card_amount` (`id` int NOT NULL AUTO_INCREMENT, `amount` int NOT NULL, `user_1` int NOT NULL, `card_1` int NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB',
+        );
+        await queryRunner.query(
+            'CREATE TABLE `calendar_event` (`id` int NOT NULL AUTO_INCREMENT, `eventStart` datetime NOT NULL, `hour` int NOT NULL, `minute` int NOT NULL, `title` varchar(255) NOT NULL, `location` varchar(255) NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB',
+        );
+        await queryRunner.query(
+            'ALTER TABLE `card` ADD CONSTRAINT `FK_b7e0fbc73a13223a3c03e7dc2c9` FOREIGN KEY (`card_set_1`) REFERENCES `card_set`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION',
+        );
+        await queryRunner.query(
+            'ALTER TABLE `card_amount` ADD CONSTRAINT `FK_0d44ec4a683c6d1bfc9ecacbe0c` FOREIGN KEY (`user_1`) REFERENCES `user`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION',
+        );
+        await queryRunner.query(
+            'ALTER TABLE `card_amount` ADD CONSTRAINT `FK_82083388ce3d2ffcbb8bfae06d4` FOREIGN KEY (`card_1`) REFERENCES `card`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION',
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(
+            'ALTER TABLE `card_amount` DROP FOREIGN KEY `FK_82083388ce3d2ffcbb8bfae06d4`',
+        );
+        await queryRunner.query(
+            'ALTER TABLE `card_amount` DROP FOREIGN KEY `FK_0d44ec4a683c6d1bfc9ecacbe0c`',
+        );
+        await queryRunner.query(
+            'ALTER TABLE `card` DROP FOREIGN KEY `FK_b7e0fbc73a13223a3c03e7dc2c9`',
+        );
+        await queryRunner.query('DROP TABLE `calendar_event`');
+        await queryRunner.query('DROP TABLE `card_amount`');
+        await queryRunner.query('DROP TABLE `card`');
+        await queryRunner.query('DROP TABLE `card_set`');
+    }
+}
