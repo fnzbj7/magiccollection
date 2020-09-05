66a63f0fd4c6b2ffc150f61322b4c94c5c6ad0e2
Árpás Milán <jukijun91@gmail.com>
2020-09-05 14:29:05 +0200

    Fix small Jest problem

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from './../src/backend/app.module';
 
 describe('AppController (e2e)', () => {
-    let app;
+    let app: any;
 
     beforeEach(async () => {
         const moduleFixture: TestingModule = await Test.createTestingModule({
