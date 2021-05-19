eff31efeff212a5353032e9189fa2c154790ed80
Árpás Milán <jukijun91@gmail.com>
2021-05-19 10:53:59 +0200

    Removing hammer js and putting there vanilla js swipe

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,27 +1,8 @@
 import { Injectable, NgModule } from '@angular/core';
-import {
-    HammerGestureConfig,
-    HammerModule,
-    HAMMER_GESTURE_CONFIG,
-} from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
-@Injectable()
-export class MyHammerConfig extends HammerGestureConfig {
-    overrides = {
-        swipe: { enable: true, direction: Hammer.DIRECTION_HORIZONTAL },
-        pinch: { enable: false },
-        rotate: { enable: false },
-    };
-}
 @NgModule({
-    imports: [FontAwesomeModule, HammerModule],
+    imports: [FontAwesomeModule],
     exports: [FontAwesomeModule],
-    providers: [
-        {
-            provide: HAMMER_GESTURE_CONFIG,
-            useClass: MyHammerConfig,
-        },
-    ],
 })
 export class SharedModule {}
