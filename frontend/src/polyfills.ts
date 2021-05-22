baec471d58617893f66a6cfc3e61ea1c1b496118
Árpás Milán <jukijun91@gmail.com>
2021-05-22 22:57:11 +0200

    Frist step updateing angular to v12

--- frontend/src/polyfills.ts
+++ frontend/src/polyfills.ts
@@ -61,7 +61,7 @@ import '@angular/localize/init';
  * Zone JS is required by default for Angular itself.
  */
 /* eslint-disable import/no-unassigned-import */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'zone.js';  // Included with Angular CLI.
 
 
 /***************************************************************************************************
