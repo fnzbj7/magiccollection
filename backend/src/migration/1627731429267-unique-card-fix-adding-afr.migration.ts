66d5fb0cc98d402678e58b69e449042011fdd885
Árpás Milán <jukijun91@gmail.com>
2021-08-04 13:44:19 +0200

    patch fix

--- backend/src/migration/1627731429267-unique-card-fix-adding-afr.migration.ts
+++ backend/src/migration/1627731429267-unique-card-fix-adding-afr.migration.ts
@@ -1,4 +1,4 @@
-import { IsNull, MigrationInterface, QueryRunner } from 'typeorm';
+import { MigrationInterface, QueryRunner } from 'typeorm';
 import { Card } from '../card/entity/card.entity';
 import { UniqueCard } from '../card/entity/unique-card.entity';
 
@@ -7,8 +7,8 @@ export class uniqueCardFixAddingAfr1627731429267 implements MigrationInterface {
         const cardName = await queryRunner.manager
             .createQueryBuilder<Card>('Card', 'a')
             .select()
-            .leftJoin('unique_card', 'b', 'a.name = b.card_name')
-            .where({ uniqueCard: IsNull() })
+            .leftJoin(UniqueCard, 'b', 'a.name = b.card_name')
+            .where('b.id is null')
             .groupBy('a.name')
             .getMany();
 
@@ -21,6 +21,7 @@ export class uniqueCardFixAddingAfr1627731429267 implements MigrationInterface {
             .into(UniqueCard)
             .values(uniqueCardName)
             .execute();
+
         await queryRunner.query(
             'Update card a inner join unique_card b on a.name= b.card_name set a.unique_card_1 = b.id where a.unique_card_1 is null',
         );
