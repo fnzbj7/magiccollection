93a2dced21e09c9aa3fb1ef8938525102bca70da
Arpas Milan <arpas.milan@eing.tigra.hu>
2022-04-18 13:35:59 +0200

    Backend type and lang

--- backend/src/card/dto/card-quantity.model.ts
+++ backend/src/card/dto/card-quantity.model.ts
@@ -1,5 +1,10 @@
+import { CardVariantType } from '../entity/card-variant-type.enum';
+import { CardLangEnum } from './card-lang.enum';
+
 export class CardQuantity {
     cardNumber: number;
     cardQuantity: number;
     cardQuantityFoil: number;
+    language: CardLangEnum;
+    type: CardVariantType;
 }
