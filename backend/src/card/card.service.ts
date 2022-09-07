dc8f6d17a5a57a705086f19fc7ca8c767f1a32bb
Árpás Milán <jukijun91@gmail.com>
2022-09-07 11:54:46 +0200

    Adding addPossibleCardtype endpoint

--- backend/src/card/card.service.ts
+++ backend/src/card/card.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Card } from './entity/card.entity';
 import { User } from '../auth/entity/user.entity';
@@ -6,6 +6,8 @@ import { CardRepository } from './card.repository';
 import { CardAmountDto } from './dto/card-amount.dto';
 import { ModifyCardDto } from './dto/add-card.dto';
 import { AllVersionCardDto } from './dto/all-version-card.dto';
+import { AddPosibleCardVariationDto } from './card.controller';
+import { PossibleCardVariation } from './entity/possible-card-variation.entity';
 
 @Injectable()
 export class CardService {
@@ -54,6 +56,45 @@ export class CardService {
     async getAllVersionForCardWithUser(uniqueCardId: number): Promise<CardAmountDto[]> {
         const cardList = await this.cardRepository.getAllVersionForCard(uniqueCardId);
         return this.convertToCardAmountDto(cardList);
+    }
+
+    async addPosibleCardVariation(
+        addPosibleCardVariationDto: AddPosibleCardVariationDto,
+    ): Promise<void> {
+        const foundCard = await this.cardRepository.findOne(addPosibleCardVariationDto.cardId, {
+            relations: ['possibleCardVariation'],
+        });
+        if (!foundCard) {
+            this.logger.error(
+                `We coud not found card with id [${addPosibleCardVariationDto.cardId}]`,
+            );
+            throw new InternalServerErrorException();
+        }
+
+        const foundType = foundCard.possibleCardVariation.find(
+            p => p.cardVariantType === addPosibleCardVariationDto.cardVariantType,
+        );
+        if (foundType) {
+            this.logger.error(
+                `Card with id [${addPosibleCardVariationDto.cardId}] already has possible card variation [id: ${foundType.id}, cardVariantType: ${foundType.cardVariantType}]`,
+            );
+            throw new InternalServerErrorException();
+        }
+
+        const possCardVar = this.cardRepository.manager
+            .getRepository<PossibleCardVariation>(PossibleCardVariation)
+            .create();
+        possCardVar.card = foundCard;
+        possCardVar.cardVariantType = addPosibleCardVariationDto.cardVariantType;
+        possCardVar.hasNormal = addPosibleCardVariationDto.hasNormal;
+        possCardVar.hasFoil = addPosibleCardVariationDto.hasFoil;
+
+        await possCardVar.save();
+        this.logger.log(
+            `Created new possible card [id: ${possCardVar.id}, possibleType: ${possCardVar.cardVariantType}, ` +
+                `hasNormal: ${possCardVar.hasNormal}, hasFoil: ${possCardVar.hasFoil}, ` +
+                `card:[id: ${possCardVar.card.id}, cardName: ${possCardVar.card.name}, cardNum: ${possCardVar.card.cardNumber}] cardId: ${possCardVar.card.id}]`,
+        );
     }
 
     private convertToCardAmountDto(cardList: Card[]): CardAmountDto[] {
