4e4bcf79982d6ace66fe99f9d8e333375c10ea40
Arpas Milan <arpas.milan@eing.tigra.hu>
2022-09-12 08:58:54 +0200

    Card selection with preview

--- frontend/src/app/magic/card-variation/card-variation.component.ts
+++ frontend/src/app/magic/card-variation/card-variation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Card } from 'src/app/model/card.model';
 import { MagicCardsListService } from '../magic-card-list/magic-cards-list.service';
 import { MagicSet } from '../magic-card-list/model/magic-set.model';
 
@@ -9,10 +10,42 @@ import { MagicSet } from '../magic-card-list/model/magic-set.model';
 })
 export class CardCariationComponent implements OnInit {
     magicSets!: MagicSet[];
+    cardSet?: string;
+    maxCardNum?: number;
+    selectedCardNum?: string;
+    cardPreview?: Card;
+
+    Arr = Array;
 
     constructor(private magicCardsListService: MagicCardsListService) {}
 
     ngOnInit(): void {
         this.magicSets = this.magicCardsListService.magicSetArray;
+        this.cardSet = ''; // this.magicSets[0].name
+    }
+
+    onCardSetChange(newValue: string) {
+        console.log(newValue);
+        this.cardSet = newValue;
+
+        const magicSet = this.magicSets.find(s => s.name === this.cardSet);
+        this.maxCardNum = magicSet?.maxNum;
+    }
+
+    onCardNumChange(newValue: string) {
+        // TODO akáűr megjeleníteni a normál kártáy
+        this.selectedCardNum = newValue;
+        if (this.selectedCardNum && this.cardSet) {
+            this.cardPreview = this.createCardPrev(this.selectedCardNum, this.cardSet);
+        }
+    }
+
+    private createCardPrev(cardNum: string, cardSet: string): Card {
+        const lastCard = new Card();
+        lastCard.cardExpansion = cardSet;
+        lastCard.cardNumber = cardNum.padStart(3, '0');
+        lastCard.cardAmount = 1;
+        lastCard.cardAmountFoil = 0;
+        return lastCard;
     }
 }
